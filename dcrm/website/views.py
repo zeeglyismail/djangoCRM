8e3c245613b6ad7151be36fb118e5f05dd24d1c8
MD. ISMAIL HOSSAIN <zeeglyismail@gmail.com>
2024-11-17 15:28:03 +0600

    register added

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     #Check to see if logging in
@@ -23,3 +24,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You Have been Logged Out....!")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login the user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error with your registration. Please try again.")
+    else:
+        form = SignUpForm()
+    
+    # Ensure the form is rendered in both GET and invalid POST scenarios
+    return render(request, 'register.html', {'form': form})
